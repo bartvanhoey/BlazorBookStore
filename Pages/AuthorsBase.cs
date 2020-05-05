3546e3c028b3b94976797d03b2709509fd94cb8b
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-05 21:57:31 +0200

    end-EP06

--- Pages/AuthorsBase.cs
+++ Pages/AuthorsBase.cs
@@ -3,28 +3,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using BlazorBookStore.Data;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlazorBookStore.Pages
 {
     public class AuthorsBase : ComponentBase
     {
-         [Inject]
-        public IAuthorService AuthorService { get; set; }    
-         public List<Author> Authors { get; set; } = new List<Author>();
+        [Inject]
+        public IAuthorService AuthorService { get; set; }
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+        public List<Author> Authors { get; set; } = new List<Author>();
 
         public Author Author { get; set; } = new Author();
-       
-        protected override async Task OnInitializedAsync()  {
-                await LoadAuthors();
+        protected ElementReference firstNameRef;
+        public string[] Cities { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadAuthors();
         }
 
-        public async Task SaveAUthorAsync(){
-           await AuthorService.SaveAuthor(Author);
-           await LoadAuthors();
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && Cities == null)
+            {
+                Cities = await JSRuntime.InvokeAsync<string[]>("getCities");
+                StateHasChanged();
+            }
+        }
+
+        public async Task SaveAuthorAsync()
+        {
+            await AuthorService.SaveAuthor(Author);
+            await LoadAuthors();
+            var firstName = Author.FirstName;
+            var lastName = Author.LastName;
             Author = new Author();
+            await JSRuntime.InvokeVoidAsync("saveMessage", firstName, lastName);
+            await JSRuntime.InvokeVoidAsync("setFocus", firstNameRef);
         }
 
-        private async Task LoadAuthors(){
+        private async Task LoadAuthors()
+        {
             Authors = await AuthorService.GetAuthors();
             StateHasChanged();
 
