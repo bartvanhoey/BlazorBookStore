da0b5304c0c1eb09dca0499d1a016d8076cd34d7
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-10 17:53:28 +0200

    end-EP13

--- BookStore.API/Migrations/20200510145519_confirmpassword.Designer.cs
+++ BookStore.API/Migrations/20200510145519_confirmpassword.Designer.cs
@@ -4,14 +4,16 @@ using BookStore.API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookStore.API.Migrations
 {
     [DbContext(typeof(BookStoreDBContext))]
-    partial class BookStoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200510145519_confirmpassword")]
+    partial class confirmpassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BookStore.Model/User.cs
+++ BookStore.Model/User.cs
@@ -13,6 +13,7 @@ namespace BookStore.Model
         public int UserId { get; set; }
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public string Source { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
