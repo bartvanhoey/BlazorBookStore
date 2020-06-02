22fc630170277379ce0fdea5113e9be579f24e2d
bartvanhoey <bartvanhoey@hotmail.com>
2020-06-02 20:21:46 +0200

    end-EP22

--- BookStore.Components/SelectCityBase.cs
+++ BookStore.Components/SelectCityBase.cs
@@ -6,14 +6,11 @@ namespace BookStore.Components
 {
     public class SelectCityBase : ComponentBase
     {
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
-
-        [Parameter]
-        public string CurrentCity { get; set; }
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+        [Parameter] public string CurrentCity { get; set; }
+        [Parameter]public EventCallback<ChangeEventArgs> OnChangeEvent { get; set; }
         public string[] Cities { get; set; }
-        [Parameter]
-        public EventCallback<ChangeEventArgs> OnChangeEvent { get; set; }
+        
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender && Cities == null)
